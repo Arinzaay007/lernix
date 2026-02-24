d6eb1ffedb55be11810be70efe85db0590c74da2
Arinzaay007 <daveedlueez@gmail.com>
2026-02-24 16:18:42 +0100

    fix: simplify next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  output: 'standalone',
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
